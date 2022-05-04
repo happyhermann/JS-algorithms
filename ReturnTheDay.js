6f4aac33a18f87c91ce4a8f36f3aaa6c3784fad9
happyhermann <carrick035@gmail.com>
2022-05-04 15:59:04 +0900

    implemented ReturnTheDay code for study

---
+++ ReturnTheDay.js
@@ -0,0 +1,59 @@
+//나의 풀이 
+
+// const result = document.getElementById('result');
+// const input = document.getElementById('input');
+
+// const weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+
+ 
+ 
+// input.addEventListener('keyup', () => {
+//     const inputValue = document.getElementById('input').value;
+//     var outValue = result.innerText = Number(inputValue);
+//     outDays(outValue);
+     
+
+// });
+
+// function outDays (outValue) { 
+//     switch(outValue) {
+//         case 1: result.innerText = `${weekdays[0]}`; break;
+//         case 2: result.innerText = `${weekdays[1]}`; break;
+//         case 3: result.innerText = `${weekdays[2]}`; break; 
+//         case 4: result.innerText = `${weekdays[3]}`; break; 
+//         case 5: result.innerText = `${weekdays[4]}`; break; 
+//         case 6: result.innerText = `${weekdays[5]}`; break; 
+//         case 7: result.innerText = `${weekdays[6]}`; break; 
+//       };
+//  };
+
+
+
+// // let number = Number(prompt("숫자를 적으세요"));
+ 
+   
+ 
+//사이트 답안 
+
+// function whatday(num) { 
+//     switch(num) {
+//       case 1:
+//         return "Sunday";
+//       case 2:
+//         return "Monday";
+//       case 3:
+//         return "Tuesday";
+//       case 4:
+//         return "Wednesday";
+//       case 5:
+//         return "Thursday";
+//       case 6:
+//         return "Friday";
+//       case 7:
+//         return "Saturday";
+//       default:
+//         return 'Wrong, please enter a number between 1 and 7';
+//     }
+//   }
+
+ 
